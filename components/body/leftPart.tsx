271506c22060d201fc010a4e4f4618ae6bd3b191
Yavuz Efe <yavuzefefindikli8@gmail.com>
2022-08-31 12:55:12 +0300

    design change 🚀

--- components/body/leftPart.tsx
+++ components/body/leftPart.tsx
@@ -3,9 +3,9 @@ import Button from "../button";
 export default function LeftPart() {
     return (
         <div className="lg:text-xl lg:ml-24 lg:w-auto w-screen ml-0 text-white flex items-center flex-col">
-            <span>share your awesome photos on <span className="text-primary border-b-2">media</span></span>
+            <span><span className="border-b-2 border-primary">share your awesome photos on</span><span className="text-primary"> media</span></span>
 
-            <span className="lg:mt-28 mt-36">start <span className="text-primary border-b-2">now</span></span>
+            <span className="lg:mt-28 mt-36"><span className="border-b-2 border-primary">start</span> <span className="text-primary"> now</span></span>
 
             <div className="lg:mt-28 mt-36 flex gap-x-8">
                 <Button>Log in</Button>
