16f73985e845d2d33a6b6d14213b1be1a0473a14
Yavuz Efe <yavuzefefindikli8@gmail.com>
2022-08-31 12:57:56 +0300

    design change v2 🚀

--- components/body/leftPart.tsx
+++ components/body/leftPart.tsx
@@ -5,7 +5,7 @@ export default function LeftPart() {
         <div className="lg:text-xl lg:ml-24 lg:w-auto w-screen ml-0 text-white flex items-center flex-col">
             <span><span className="border-b-2 border-primary">share your awesome photos on</span><span className="text-primary"> media</span></span>
 
-            <span className="lg:mt-28 mt-36"><span className="border-b-2 border-primary">start</span> <span className="text-primary"> now</span></span>
+            <span className="lg:mt-28 mt-36"><span className="text-primary" >start</span> <span className="border-b-2 border-primary">now</span></span>
 
             <div className="lg:mt-28 mt-36 flex gap-x-8">
                 <Button>Log in</Button>
